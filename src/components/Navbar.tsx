e08e0a372d01554e50768d01f17c8cde15b266bb
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-08-30 17:31:14 +0300

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+	return (
+		<nav>
+			<h1>404 Store</h1>
+			<div className='navbarMenu'>
+				<a href='/'>Home</a>
+				<a href='/store'>Store</a>
+				<a href='/cart'>cart</a>
+				<a href='/about'>About</a>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
