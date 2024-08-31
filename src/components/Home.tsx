fdb5b4673ddce8b1b7ab5c0a455f11671cc69992
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-01 00:01:00 +0300

    Add Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+	return (
+		<main>
+			<h2>Home</h2>
+			<p>This is the Home page</p>
+			<p>
+				You can shop for more items if you go to the Store page. You can
+				view items in the cart on the Cart page. You can read more about
+				the project on the About page
+			</p>
+		</main>
+	);
+}
+
+export default Home;
