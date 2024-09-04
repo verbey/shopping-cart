e6701546d1670fceee44e988a7491d196c6775bc
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-04 05:02:55 +0300

    Add tests for Navbar to have link elements

--- tests/navbar.test.tsx
+++ tests/navbar.test.tsx
@@ -1,13 +1,29 @@
-import { describe, test } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from '../src/components/Navbar';
 
 describe('Navbar component', () => {
 	test('Renders', () => {
-		render(<Navbar />);
+		render(
+			<BrowserRouter>
+				<Navbar />
+			</BrowserRouter>
+		);
 
-		screen.debug();
+		expect(screen.getByText('404 Store')).toBeVisible();
+	});
+	test('Has link elements', () => {
+		render(
+			<BrowserRouter>
+				<Navbar />
+			</BrowserRouter>
+		);
+		expect(screen.getByText('Home')).toHaveAttribute('href', '/');
+		expect(screen.getByText('Store')).toHaveAttribute('href', '/store');
+		expect(screen.getByText('Cart')).toHaveAttribute('href', '/cart');
+		expect(screen.getByText('About')).toHaveAttribute('href', '/about');
 	});
 });
