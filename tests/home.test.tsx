cf0c6ee3f45b140d94c09e171cc1baef9d6b0caa
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-04 04:31:49 +0300

    Switch from it to test

--- tests/home.test.tsx
+++ tests/home.test.tsx
@@ -1,15 +1,15 @@
-import { describe, it, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import Home from '../src/components/Home';
 
 describe('Home component', () => {
-	it('Renders', () => {
+	test('Renders', () => {
 		render(<Home />);
 		screen.debug();
 	});
-	it('Has text elements', () => {
+	test('Has text elements', () => {
 		render(<Home />);
 		expect(screen.getByText('This is the Home page')).toBeVisible();
 		expect(screen.getByText(/You can shop/i)).toBeVisible();
