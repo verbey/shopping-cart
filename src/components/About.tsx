37b29046881682f7811dde95ad4b4c228dcc21ef
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-04 05:38:48 +0300

    Add About component

---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+function About() {
+	return (
+		<main>
+			<h2>About</h2>
+			<p>This is the About page</p>
+			<p>
+				A project to practice learned React concepts. Made as a part of{' '}
+				<a href='https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart'>
+					the Odin Project course
+				</a>
+				. See
+				<a href='https://codeberg.org/Verbiturum/shopping-card'>
+					the code and README.md
+				</a>
+				for more informatin.
+			</p>
+		</main>
+	);
+}
+
+export default About;
