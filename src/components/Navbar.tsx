58c1ba8c33c04352c199879ad1e03133b26c9c53
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-11 03:34:05 +0300

    Make Navbar display number of items in cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ shoppingCartItems }: { shoppingCartItems: number }) {
 	return (
 		<nav>
 			<h1>404 Store</h1>
@@ -8,6 +8,7 @@ function Navbar() {
 				<Link to='/'>Home</Link>
 				<Link to='/store'>Store</Link>
 				<Link to='/cart'>Cart</Link>
+				<div className='shoppingCartItems'>{shoppingCartItems}</div>
 				<Link to='/about'>About</Link>
 			</div>
 		</nav>
