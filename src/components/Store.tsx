1a5f15a88527653bbaf6cf84b0c1a259bb5a9b65
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-13 01:41:07 +0300

    Add Store item fetching and display logic

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from 'react';
+import ShoppingItem from './ShoppingItem';
+
+interface StoreData {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+	rating: {
+		count: number;
+		rate: number;
+	};
+}
+
 function Store() {
+	const [storeItems, setStoreItems] = useState<StoreData[] | null>(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch('https://fakestoreapi.com/products?limit=10', { mode: 'cors' })
+			.then((response) => response.json())
+			.then((response) => {
+				setStoreItems(response);
+			})
+			.catch((error) => {
+				console.log(error);
+				setError(error);
+			});
+	}, []);
+
 	return (
 		<main>
 			<h2>Store</h2>
+			{storeItems && (
+				<ul>
+					{storeItems.map((item) => (
+						<li key={item.id}>
+							<ShoppingItem
+								title={item.title}
+								image={item.image}
+								quantity={1}
+								informationalOnly={false}
+							/>
+						</li>
+					))}
+				</ul>
+			)}
+
+			{error && (
+				<p>
+					Failed to load store items. See the console for more
+					information.
+				</p>
+			)}
 		</main>
 	);
 }
