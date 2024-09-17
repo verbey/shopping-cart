b90021d2b08ade8e34cdc2b39677f96e0f9da01b
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-18 01:40:09 +0300

    Add Cart component tests

---
+++ tests/cart.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect } from 'vitest';
+import React from 'react';
+
+import Cart from '../src/components/Cart';
+
+describe('Cart component', () => {
+	test('Displays items if there are any', () => {
+		render(
+			<Cart shoppingCartItems={[{ title: 'Test Item', quantity: 1 }]} />
+		);
+		expect(screen.getByText('Test Item')).toBeInTheDocument();
+	});
+	test('Checkout button takes user to the checkout page', () => {
+		// TODO
+	});
+});
