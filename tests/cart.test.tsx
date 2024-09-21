a748425bdacd893ffff7af10f08c2e8ec6de6d2c
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-21 12:48:47 +0300

    Update Cart tests to mock context

--- tests/cart.test.tsx
+++ tests/cart.test.tsx
@@ -1,17 +1,53 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
 import React from 'react';
+import { MemoryRouter, Outlet, Route, Routes } from 'react-router-dom';
 
 import Cart from '../src/components/Cart';
 
+interface RenderRouteWithOutletContextProps<T = ShoppingCartItemsContext> {
+	context: T;
+	children: React.ReactNode;
+}
+
+const RenderRouteWithOutletContext = <T,>({
+	context,
+	children,
+}: RenderRouteWithOutletContextProps<T>) => {
+	return (
+		<MemoryRouter>
+			<Routes>
+				<Route path='/' element={<Outlet context={context as T} />}>
+					<Route index element={children} />
+				</Route>
+			</Routes>
+		</MemoryRouter>
+	);
+};
+
+interface ShoppingCartItems {
+	title: string;
+	image?: string;
+	quantity: number;
+	price: number;
+}
+
+type ShoppingCartItemsContext = [
+	ShoppingCartItems[] | null,
+	(items: ShoppingCartItems[] | null) => void
+];
+
 describe('Cart component', () => {
 	test('Displays items if there are any', () => {
+		const mockOutletContextData: ShoppingCartItemsContext = [
+			[{ title: 'Test Item', quantity: 1, price: 10 }],
+			() => {},
+		];
+
 		render(
-			<Cart
-				shoppingCartItems={[
-					{ title: 'Test Item', quantity: 1, price: 10 },
-				]}
-			/>
+			<RenderRouteWithOutletContext context={mockOutletContextData}>
+				<Cart />
+			</RenderRouteWithOutletContext>
 		);
 		expect(screen.getByText('Test Item')).toBeInTheDocument();
 	});
