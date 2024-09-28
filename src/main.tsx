3e510e05ae8d25f37a78d7e4412609da447d5c0e
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-28 23:55:09 +0300

    Move CSS reset and add default styling

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import Cart from './components/Cart.tsx';
 import Checkout from './components/Checkout.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './styles/reset.css';
+import './styles/style.css';
+
 const router = createBrowserRouter([
 	{
 		path: '/',
