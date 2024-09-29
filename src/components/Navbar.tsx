59eb317319d1e911324dd4c54e6c0c4bc65b2452
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-29 16:39:01 +0300

    Style Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import styles from '../styles/navbar.module.css';
+
 function Navbar({
 	shoppingCartItems,
 }: {
@@ -8,14 +10,22 @@ function Navbar({
 	return (
 		<nav>
 			<h1>404 Store</h1>
-			<div className='navbarMenu'>
-				<Link to='/'>Home</Link>
-				<Link to='/store'>Store</Link>
-				<Link to='/cart'>Cart</Link>
-				<div className='shoppingCartItems'>
+			<div className={styles.navbarMenu}>
+				<Link to='/' className={styles.link}>
+					Home
+				</Link>
+				<Link to='/store' className={styles.link}>
+					Store
+				</Link>
+				<Link to='/cart' className={styles.link}>
+					Cart
+				</Link>
+				<div className={styles.shoppingCartItems}>
 					{shoppingCartItems === undefined ? '0' : shoppingCartItems}
 				</div>
-				<Link to='/about'>About</Link>
+				<Link to='/about' className={styles.link}>
+					About
+				</Link>
 			</div>
 		</nav>
 	);
