8d0fa7a560c237a1af03362cf7a8e904a577bf4a
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-09-29 17:21:13 +0300

    Style About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,24 @@
+import generalStyles from '../styles/infoComponent.module.css';
+import aboutStyles from '../styles/about.module.css';
+
 function About() {
 	return (
-		<main>
-			<h2>About</h2>
+		<main className={generalStyles.main}>
+			<h2 className={generalStyles.title}>About</h2>
 			<p>This is the About page</p>
 			<p>
 				A project to practice learned React concepts. Made as a part of{' '}
-				<a href='https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart'>
+				<a
+					href='https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart'
+					className={aboutStyles.link}
+				>
 					the Odin Project course
 				</a>
 				. See{' '}
-				<a href='https://codeberg.org/Verbiturum/shopping-card'>
+				<a
+					href='https://codeberg.org/Verbiturum/shopping-card'
+					className={aboutStyles.link}
+				>
 					the code and README.md{' '}
 				</a>
 				for more information.
