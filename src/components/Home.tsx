6794b00911495655a9c63581bbba0633c726a7fc
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-10-05 03:17:44 +0300

    Add scroll-snapping to the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,95 @@ function Home() {
 	return (
 		<main className={styles.main}>
 			<h2 className={styles.title}>Home</h2>
-			<p>This is the Home page</p>
-			<p>
-				You can shop for more items if you go to the Store page. You can
-				view items in the cart on the Cart page. You can read more about
-				the project on the About page
-			</p>
+			<section>
+				<p>This is the Home page</p>
+				<p>
+					You can shop for more items if you go to the Store page. You
+					can view items in the cart on the Cart page. You can read
+					more about the project on the About page
+				</p>
+			</section>
+			<section>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Suspendisse tempus, lectus in cursus malesuada, erat sem
+					vehicula nisi, eu commodo lacus tortor et est. Donec vitae
+					vehicula nibh, quis iaculis nunc. Sed sed nunc ipsum.
+					Integer malesuada risus id nibh dictum, a auctor lectus
+					ultrices. Nullam sit amet leo in nunc lacinia tincidunt. Sed
+					non malesuada risus, eu viverra dolor. Nulla euismod, magna
+					sed malesuada convallis, velit elit egestas nunc, ac
+					scelerisque lorem metus et nibh. Lorem ipsum odor amet,
+					consectetuer adipiscing elit. Congue integer suscipit
+					imperdiet justo nostra condimentum ac nunc. Praesent
+					vestibulum habitant parturient ridiculus primis, efficitur
+					lectus lectus. Parturient mi eleifend erat varius maecenas
+					gravida natoque phasellus. Nam eleifend bibendum consequat
+					gravida tristique molestie. Sed aptent nam proin dictumst
+					tempus consectetur fames sollicitudin senectus. Morbi enim
+					auctor dolor imperdiet erat consequat consequat nullam?
+					Tempus blandit dignissim duis diam sagittis ad feugiat.
+					Acommodo magna euismod suspendisse ultrices vulputate
+					malesuada taciti. Sem maximus non cras potenti potenti
+					blandit ornare vel. Turpis odio fringilla tempor eros,
+					placerat ridiculus erat taciti. Proin eleifend vestibulum
+					conubia pellentesque a dui. Id et augue auctor morbi eros.
+				</p>
+			</section>
+			<section>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Suspendisse tempus, lectus in cursus malesuada, erat sem
+					vehicula nisi, eu commodo lacus tortor et est. Donec vitae
+					vehicula nibh, quis iaculis nunc. Sed sed nunc ipsum.
+					Integer malesuada risus id nibh dictum, a auctor lectus
+					ultrices. Nullam sit amet leo in nunc lacinia tincidunt. Sed
+					non malesuada risus, eu viverra dolor. Nulla euismod, magna
+					sed malesuada convallis, velit elit egestas nunc, ac
+					scelerisque lorem metus et nibh. Lorem ipsum odor amet,
+					consectetuer adipiscing elit. Congue integer suscipit
+					imperdiet justo nostra condimentum ac nunc. Praesent
+					vestibulum habitant parturient ridiculus primis, efficitur
+					lectus lectus. Parturient mi eleifend erat varius maecenas
+					gravida natoque phasellus. Nam eleifend bibendum consequat
+					gravida tristique molestie. Sed aptent nam proin dictumst
+					tempus consectetur fames sollicitudin senectus. Morbi enim
+					auctor dolor imperdiet erat consequat consequat nullam?
+					Tempus blandit dignissim duis diam sagittis ad feugiat.
+					Acommodo magna euismod suspendisse ultrices vulputate
+					malesuada taciti. Sem maximus non cras potenti potenti
+					blandit ornare vel. Turpis odio fringilla tempor eros,
+					placerat ridiculus erat taciti. Proin eleifend vestibulum
+					conubia pellentesque a dui. Id et augue auctor morbi eros.
+				</p>
+			</section>
+			<section>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Suspendisse tempus, lectus in cursus malesuada, erat sem
+					vehicula nisi, eu commodo lacus tortor et est. Donec vitae
+					vehicula nibh, quis iaculis nunc. Sed sed nunc ipsum.
+					Integer malesuada risus id nibh dictum, a auctor lectus
+					ultrices. Nullam sit amet leo in nunc lacinia tincidunt. Sed
+					non malesuada risus, eu viverra dolor. Nulla euismod, magna
+					sed malesuada convallis, velit elit egestas nunc, ac
+					scelerisque lorem metus et nibh. Lorem ipsum odor amet,
+					consectetuer adipiscing elit. Congue integer suscipit
+					imperdiet justo nostra condimentum ac nunc. Praesent
+					vestibulum habitant parturient ridiculus primis, efficitur
+					lectus lectus. Parturient mi eleifend erat varius maecenas
+					gravida natoque phasellus. Nam eleifend bibendum consequat
+					gravida tristique molestie. Sed aptent nam proin dictumst
+					tempus consectetur fames sollicitudin senectus. Morbi enim
+					auctor dolor imperdiet erat consequat consequat nullam?
+					Tempus blandit dignissim duis diam sagittis ad feugiat.
+					Acommodo magna euismod suspendisse ultrices vulputate
+					malesuada taciti. Sem maximus non cras potenti potenti
+					blandit ornare vel. Turpis odio fringilla tempor eros,
+					placerat ridiculus erat taciti. Proin eleifend vestibulum
+					conubia pellentesque a dui. Id et augue auctor morbi eros.
+				</p>
+			</section>
 		</main>
 	);
 }
